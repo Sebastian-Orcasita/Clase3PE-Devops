e4e7adb14d36f231b1fbaf7e24d2f018f92b3941
Sebastian Orcasita <sebastian.orcasita@upb.edu.co>
2026-01-27 18:36:14 -0500

    Version 1

---
+++ src/main/java/com/example/Clase3_Devops/Clase3DevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Clase3_Devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase3DevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clase3DevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Clase3_Devops/Clase3DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Clase3_Devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase3DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
